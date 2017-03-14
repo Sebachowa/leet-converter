2ae071a01e7e2c3616dd1c90ecacd4d3c35c2905
Sebastian Nugnez <sebastian.nugnez@gmail.com>
2017-03-14 09:06:05 +0800

    bootstrap loaded

--- app/controllers/converter_controller.rb
+++ app/controllers/converter_controller.rb
@@ -1,2 +1,14 @@
 class ConverterController < ApplicationController
+
+def word
+# Input the word
+# Send to translate
+end
+
+def translate
+# Do the translation
+
+end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get '/word', to: 'converter#word'
+  get '/translate', to: 'converter#translate'
+
+  root to: 'converter#word'
 end
