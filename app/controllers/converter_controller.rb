d3e6a4e2045ed45e0cbf2be1ad796e59d83a3bdf
Sebastian Nugnez <sebastian.nugnez@gmail.com>
2017-03-14 20:53:37 +0800

    done. working in styling

--- app/controllers/converter_controller.rb
+++ app/controllers/converter_controller.rb
@@ -1,14 +1,40 @@
 class ConverterController < ApplicationController
 
-def word
-# Input the word
-# Send to translate
-end
+  def word
+  # Input the word
+  # Send to translate
+  end
 
-def translate
-# Do the translation
+  def translate
+  # Do the translation
+    word = params[:word]
+    leet_array = []
+    word.downcase.split("").each do |letter|
+      if letter == "a"
+        leet_array << "4"
+      elsif letter == "b"
+        leet_array << '8'
+      elsif letter == "e"
+        leet_array << "3"
+      elsif letter == "g"
+        leet_array << "6"
+      elsif letter == "i"
+        leet_array << "1"
+      elsif letter == "l"
+        leet_array << "|"
+      elsif letter == "o"
+        leet_array << "0"
+      elsif letter == "s"
+        leet_array << "5"
+      elsif letter == "t"
+        leet_array << "7"
+      else
+        leet_array <<letter
+        end
+      end
+    @translation = leet_array.join("")
 
-end
+  end
 
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'converter#word'
 
   get '/word', to: 'converter#word'
   get '/translate', to: 'converter#translate'
 
-  root to: 'converter#word'
+
 end
